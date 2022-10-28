0d740b4e89cc8958f22da2758e0f748409bd0839
Dani <dani.webdev@gmail.com>
2022-10-28 13:50:41 +0700

    update save action config

--- views/user/config.php
+++ views/user/config.php
@@ -33,14 +33,16 @@
             success: function(res) {
                 console.log(res)
 
+                $('table tbody').html('')
+
                 res.details.forEach((item, i) => {
                     $('table tbody').append(`
                         <tr>
                             <td><input class="form-control" name="key" value="${item.key}"></td>
                             <td><input class="form-control" name="value" value="${item.value}"></td>
                             <td>
-                                <button class="btn btn-primary">S</button>
-                                <button class="btn btn-danger" data-id="${item.id}">D</button>
+                                <button class="btn btn-primary save-config">S</button>
+                                <button class="btn btn-danger delete" data-id="${item.id}">D</button>
                             </td>
                         </tr>
                     `);
@@ -58,11 +60,66 @@
                     <td><input class="form-control" name="key" value=""></td>
                     <td><input class="form-control" name="value" value=""></td>
                     <td>
-                        <button class="btn btn-primary">S</button>
-                        <button class="btn btn-danger">D</button>
+                        <button class="btn btn-primary save-config">S</button>
+                        <button class="btn btn-danger delete">D</button>
                     </td>
                 </tr>
             `);
+        });
+
+
+        $(document).on('click', '.delete', function() {
+            let c = confirm("Anda yakin hapus config ini ?");
+            let id = $(this).data('id');
+
+            if(c) {
+                if(id == undefined) {
+                    $(this).closest('tr').remove();
+                } else {
+                    let el = this;
+                    $.ajax({
+                        method: "DELETE",
+                        url: '/api/config/'+id,
+                        headers: {
+                            "Authorization": "Bearer "+localStorage.getItem('token')
+                        },
+                        success: function(res) {
+                            console.log(res);
+                            alert("Berhasil")
+                            $(el).closest('tr').remove();
+                        }
+                    })
+                }
+            }
+        });
+
+        $(document).on('click', '.save-config', function() {
+            let key = $(this).closest('tr').find('[name="key"]').val();
+            let value = $(this).closest('tr').find('[name="value"]').val()  ;
+            console.log(key, value);
+
+            data = {};
+
+            data[key] = value;
+
+
+            let c = confirm('Anda yakin ?');
+
+            if(c) {
+                $.ajax({
+                    contentType: 'application/json',
+                    data: JSON.stringify(data),
+                    method: "POST",
+                    url: '/api/config',
+                    headers: {
+                        "Authorization": "Bearer "+localStorage.getItem('token')
+                    },
+                    success: function(res) {
+                        console.log(res);
+                        alert("Berhasil")
+                    }
+                })
+            }
         })
     }
 </script>
